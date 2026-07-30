da30d9c2e8aad0ac861fbc1ea006b29ca381c62e
Puneet Goswami <parasgoswami1156@gmail.com>
2026-07-30 15:58:03 +0530

    fix experience timeline

--- app/page.js
+++ app/page.js
@@ -959,7 +959,7 @@ March 2019 - May 2020
       </div>
 
       <div
-    className={`w-full md:w-[45%]
+    className={`w-full md:w-[49%]
   ${
     exp.side === "left"
       ? "md:mr-auto md:pr-12"
